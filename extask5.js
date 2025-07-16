4e4d75e533cadae85adf34d12cfdfb50cb68be3c
Nagaishita <153056279+Nagaishita@users.noreply.github.com>
2025-07-16 13:00:55 +0530

    Add files via upload

---
+++ extask5.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+// Logging Middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} request made to ${req.url}`);
+  next(); // Continue to the next middleware or route
+});
+
+// Middleware to parse JSON (optional for POST)
+app.use(express.json());
+
+// GET /products
+app.get('/products', (req, res) => {
+  res.send('Here is the list of all products.');
+});
+
+// POST /products
+app.post('/products', (req, res) => {
+  res.send('A new product has been added.');
+});
+
+// GET /categories
+app.get('/categories', (req, res) => {
+  res.send('Here is the list of all categories.');
+});
+
+// POST /categories
+app.post('/categories', (req, res) => {
+  res.send('A new category has been created.');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
