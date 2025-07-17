5f1c8f9f07bfc57135eccb4e15a653593d0051c5
Nagaishita <153056279+Nagaishita@users.noreply.github.com>
2025-07-17 11:36:25 +0530

    Add files via upload
    ecommerce startup

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// Import route files
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+
+app.use(express.json()); // to parse JSON body
+
+// Mount routes
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
+app.use("/cart", cartRoutes);
+
+// Start server
 app.listen(PORT, () => {
-  console.log('Server is up and running on port 3000! Ready to handle requests.');
+  console.log(`Server is running on port ${PORT}`);
 });
